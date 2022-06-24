77672d34e232825a13db42113f7635240f97e4d2
Maks-Nikodimov <777.812@mail.ru>
2022-06-24 03:03:34 +0300

    Версионирование Terraform

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = "= 1.2.3"
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.75.0"
+    }
+  }
+
+  #  backend "s3" {
+  #    endpoint   = "storage.yandexcloud.net"
+  #    bucket     = "skillfactory-bucket"
+  #    region     = "ru-central1-a"
+  #    key        = "issue/lemp.tfstate"
+  #    access_key = "YCAJE9zFpGzR6mMalTGRs0jHX"
+  #    secret_key = "YCN8UC8Q12LwsIG0DNkijwbRalRHZPoEbUu61kgi"
+
+  #    skip_region_validation      = true
+  #    skip_credentials_validation = true
+  #  }
+}
+
+provider "yandex" {
+  token     = "AQAAAABiKzSqAATuwTR_TmWYGEnJsdMOSM4e5jk"
+  cloud_id  = "b1glcvtpbgiophljh0vm"
+  folder_id = "b1gvj6k0c2u5cdk5rh9c"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet2" {
+  name           = "subnet2"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.11.0/24"]
+}
+
+module "ya_instance_1" {
+  source                = "./modules"
+  instance_family_image = "lemp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet1.id
+}
+
+module "ya_instance_2" {
+  source                = "./modules"
+  instance_family_image = "lamp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet2.id
+}
